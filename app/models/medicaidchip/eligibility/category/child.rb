2e5c67597a3389dca424572217d1d029586b1767
Curtis <curtismorales@gmail.com>
2013-07-05 16:18:56 -0400

    Added first new ruleset

--- app/models/medicaidchip/eligibility/category/child.rb
+++ app/models/medicaidchip/eligibility/category/child.rb
@@ -2,6 +2,8 @@
 
 module Medicaidchip::Eligibility::Category
   class Child < Ruleset
+    name        "Identify Medicaid or CHIP Category - Child"
+    mandatory   "Mandatory"
     references  "§435.118 (Medicaid); 1102 of SSA (for CHIP)"
     applies_to  "Medicaid and CHIP"
     purpose     "Determine if child category applies."
@@ -10,6 +12,12 @@ module Medicaidchip::Eligibility::Category
     calculated "Applicant Age" do
       (current_date - v("Person Birth Date"))/365.25
     end
+
+    input "Applicant Age", "Calculated in Create Applicant Child List logic", "Number"
+
+    config "Child Age Threshold", "System Configuration", "Integer", %w(19)
+    config "Option Young Adults", "State Configuration", "Char(1)", %w(Y N)
+    config "Young Adult Age Threshold", "State Configuration", "Integer" %w(20 21)
 
     # Outputs
     indicator "Applicant Child Category Indicator", %w(Y N)
@@ -24,12 +32,30 @@ module Medicaidchip::Eligibility::Category
       end
     end
 
-    rule "State does not  cover young adults- Child is over 18" do
+    rule "State does not cover young adults- Child is over 18" do
+      if v("Applicant Age") >= c("Child Age Threshold") && c("Option Young Adults") == 'N'
+        o["Applicant Child Category Indicator"] = 'N'
+        o["Child Category Determination Date"] = current_date
+        o["Child Category Ineligibility Reason"] = 115
+      end
+    end
+
+    rule "State covers young adults- Child is less than age limit for young adults" do
       if c("Option Young Adults") == 'Y' && v("Applicant Age") < c("Young Adult Age Threshold")
         o["Applicant Child Category Indicator"] = 'Y'
         o["Child Category Determination Date"] = current_date
         o["Child Category Ineligibility Reason"] = 999
       end
     end
+
+    rule "State covers young adults- Child is older than age limit for young adults" do
+      if c("Option Young Adults") == 'Y' && v("Applicant Age") >= c("Young Adult Age Threshold")
+        o["Applicant Child Category Indicator"] = 'N'
+        o["Child Category Determination Date"] = current_date
+        o["Child Category Ineligibility Reason"] = 394
+      end
+    end
+
+    special_instruction "The Child Age Threshold is set as a variable for system configurability, not to support a state option.  The value will be set to 19."
   end
 end
---
+++ app/models/medicaidchip/eligibility/category/pregnant.rb
@@ -0,0 +1,38 @@
+# encoding: UTF-8
+
+module Medicaidchip::Eligibility::Category
+  class Pregnant < Ruleset
+    name        "Identify Medicaid or CHIP Category – Pregnant Women"
+    mandatory   "Mandatory"
+    references  "§435.116 (Medicaid); 1115 of SSA (for CHIP)"
+    applies_to  "Medicaid and CHIP"
+    purpose     "Determine if applicant belongs in the pregnant women category."
+    description "Women who are currently pregnant or within postpartum period are in the Medicaid or CHIP Pregnancy category until the end of the postpartum period.  The Postpartum period is defined as beginning on the date the pregnancy terminates and ending on the last day of the month in which a 60-day period (beginning on the date the pregnancy terminates) ends (§435.4)."
+
+    assumption "System logic will determine if a woman is in a postpartum period based on her attestation of pregnancy and the age of her children."
+
+    input "Applicant Pregnant Indicator", "Application", "Char(1)", %w(Y N)
+    input "Applicant Post Partum Period Indicator", "From Pregnant Woman Category Logic", "Char(1)", %w(Y N)
+
+    # Outputs
+    indicator "Applicant Pregnancy Category Indicator", %w(Y N)
+    date "Pregnancy Category Determination Date"
+    code "Pregnancy Category Ineligibility Reason", %w(999 124)
+
+    rule "Applicant is pregnant or in postpartum period" do
+      if v("Applicant Pregnant Indicator") == 'Y' || v("Applicant Post Partum Period Indicator") == 'Y'
+        o["Applicant Pregnancy Category Indicator"] = 'Y' 
+        o["Pregnancy Category Determination Date"] = current_date
+        o["Pregnancy Category Ineligibility Reason"] = 999
+      end
+    end
+
+    rule "Applicant is not pregnant or within postpartum period" do
+      if v("Applicant Pregnant Indicator") == 'N' && v("Applicant Post Partum Period Indicator") == 'N'
+        o["Applicant Pregnancy Category Indicator"] = 'N' 
+        o["Pregnancy Category Determination Date"] = current_date
+        o["Pregnancy Category Ineligibility Reason"] = 124
+      end
+    end
+  end
+end
