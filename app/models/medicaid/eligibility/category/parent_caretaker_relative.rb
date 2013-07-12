cd4b4b1855ab5c67b9b4902c53d9c4dd2bbfbcf3
Curtis <curtismorales@gmail.com>
2013-07-12 17:23:41 -0400

    Putting caretaker rule on hold

--- app/models/medicaid/eligibility/category/parent_caretaker_relative.rb
+++ app/models/medicaid/eligibility/category/parent_caretaker_relative.rb
@@ -42,7 +42,7 @@ module Medicaid::Eligibility::Category
     config "Option Caretaker Relative Relationship", "State Configuration", "Char(2)", %w(00 01 02 03 04)
 
     def calculate_child_age(person_birth_date)
-      (current_date - person_birth_date)/365.25
+      ((current_date - person_birth_date)/365.25).to_int
     end
 
     # Overwrite the Applicant Child List with the calculated ages appended for each child
@@ -93,77 +93,109 @@ module Medicaid::Eligibility::Category
         }
       }
     
-    Business Rule Logic:  For each record in the Applicant Child List determine if child meets four criteria
-    1.  Dependent Child Age Logic – Child under dependent age 
-      When Dependent Child Age < Dependent Age Threshold
-    Then Child of Caretaker Dependent Age Indicator= Y and Child of Caretaker Dependent Age Determination Date = current date and Child of Caretaker Dependent Age Ineligibility Reason = 999
-    Continue to Deprivation logic below with the current child record
-    2.  Dependent Child Age Logic – Child older than dependent age
-      When Dependent Child Age > Dependent Age Threshold
-    Then Child of Caretaker Dependent Age Indicator= N and Child of Caretaker Dependent Age Determination Date = current date and Child of Caretaker Dependent Age Ineligibility Reason = 147
-    Continue to Deprivation logic below with the current child record
-    3.  Dependent Child Age Logic – Child equal to dependent age and a student  When Option Dependent Student = Y  and  Dependent Child Age = Dependent Age Threshold and Student Indicator = Y
-    Then Child of Caretaker Dependent Age Indicator= Y and Child of Caretaker Dependent Age Determination Date = current date and Child of Caretaker Dependent Age Ineligibility Reason = 999
-    Continue to the Deprivation logic below with the current child record
-    4.  Dependent Child Age Logic – Child is equal to dependent age but not a student When Option Dependent Student = Y and Dependent Child Age = Dependent Age Threshold and Student Indicator = N
-    Then Child of Caretaker Dependent Age Indicator= N and Child of Caretaker Dependent Age Determination Date = current date and Child of Caretaker Dependent Age Ineligibility Reason = 137
-    Continue to the Deprivation logic below with the current child record
-    5.  Dependent Deprived of Parental Support – Requirement not retained by state  When Deprivation Requirement Retained = N 
-    Then Child of Caretaker Deprived Child Indicator =X and Child of Caretaker Deprived Child Determination Date = current date and  Child of Caretaker Deprived Child Ineligibility Reason = 555
-    Continue to the Relationship logic below with the current child record
+    def run(context)
+      self.class.calculateds.each do |cvar|
+        cvar.run(context)
+      end
 
-    6.  Dependent Deprived of Parental Support – Requirement retained by state  When Deprivation Requirement Retained = Y 
-    Then Child of Caretaker Deprived Child Indicator =T and Child of Caretaker Deprived Child Determination Date = current date and  Child of Caretaker Deprived Child Ineligibility Reason = 999
-    Continue to the Relationship logic below with the current child record
-    7.  Caretaker Relationship – Caretaker meets standard definition  When Option Caretaker Relative Relationship = 00 and Relationship Code(caretaker to dependent child) = (03, 07, 12, 13, 14, 15, 16, 23)
-    Then Child of Caretaker Relationship Indicator= Y and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 999
-    Get next record from Applicant Children list and begin at Dependent Age logic.
-    8.  Caretaker Relationship – Caretaker does not meet standard definition  When Option Caretaker Relative Relationship = 00 and Relationship Code(caretaker to dependent child) <> (03, 07, 12, 13, 14, 15, 16, 23)
-    Then Child of Caretaker Relationship Indicator= N and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 132
-    Get next record from Applicant Children list and begin at Dependent Age logic.
-    9.  Caretaker Relationship – Caretaker meets any relative definition  When Option Caretaker Relative Relationship = 01 and Relationship Code(caretaker to dependent child) = (03, 07, 12, 13, 14, 15, 16, 23, 27, 30)
-    Then Child of Caretaker Relationship Indicator= Y and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 999
-    Get next record from Applicant Children list and begin at Dependent Age logic.
-    10. Caretaker Relationship – Caretaker does not meet any relative definition  When Option Caretaker Relative Relationship = 01 and Relationship Code(caretaker to dependent child) <>(03, 07, 12, 13, 14, 15, 16, 23, 27, 30)
-    Then Child of Caretaker Relationship Indicator= N and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 131
-    Get next record from Applicant Children list and begin at Dependent Age logic.
-    11. Caretaker Relationship – Any Adult – Caretaker is an adult  When Option Caretaker Relative Relationship = 04 and Applicant Age >= Child Age Threshold
-    Then Child of Caretaker Relationship Indicator= Y and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 999
-    Get next record from Applicant Children list and begin at Dependent Age logic.
-    12. Caretaker Relationship – Any Adult – Caretaker is not an Adult  When Option Caretaker Relative Relationship = 04 and Applicant Age < Child Age Threshold
-    Then Child of Caretaker Relationship Indicator= N and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 130
-    Get next record from Applicant Children list and begin at Dependent Age logic.
-    13. Caretaker Relationship – Domestic Partner – Individual is domestic partner  When Option Caretaker Relative Relationship = 02 and Relationship Code(to child) = 17
-    Then Child of Caretaker Relationship Indicator= Y and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 999
-    Get next record from Applicant Children list and begin at Dependent Age logic.
-    14. Caretaker Relationship – Domestic Partner Individual is not the domestic partner  When Option Caretaker Relative Relationship = 02 and Relationship Code(to child) <> 17
-    Then Child of Caretaker Relationship Indicator= N and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 389
-    Get next record from Applicant Children list and begin at Dependent Age logic.
+      context.o["Qualified Children List"] = Array.new(context.v("Applicant Child List").length, {})
+      
+      self.class.rules.each do |rule|
+        rule.run(context)
+      end
 
-    Business Rule Logic: Set Parent Caretaker Category
-    1.  Applicant Child list is empty – no children   When Applicant Children List is empty
-    Then Applicant Parent Caretaker Category Indicator = N and Parent Caretaker Category Determination Date = current date and Parent Caretaker Category Ineligibility Reason = 146
-    2.  Child of Caretaker meets all criteria When Child of Caretaker Dependent Age Indicator= Y and Child of Caretaker Relationship Indicator= Y and Child of Caretaker Deprived Child Indicator = (X or T) 
-    Add child record to the Qualified Children list and get next child record
-    3.  Child of Caretaker does not meet all criteria When Child of Caretaker Dependent Age Indicator= N or Child of Caretaker Deprived Child Indicator= N or Child of Caretaker Relationship Indicator= N
-    Then get next child record 
-    4.  No more child records When Child of Caretaker Deprived Child Indicator=  T (for all records in Qualified Children list)
-    Then Applicant Parent Caretaker Category Indicator = T and Parent Caretaker Category Determination Date = current date and Parent Caretaker Category Ineligibility Reason = 999 
-    5.  No more child records When Child of Caretaker Deprived Child Indicator <>  T (for all records in Qualified Children list)
-    Then Applicant Parent Caretaker Category Indicator = Y and Parent Caretaker Category Determination Date = current date and Parent Caretaker Category Ineligibility Reason = 999 
-    6.  No more child records When Qualified Children List is empty
-    Then Applicant Parent Caretaker Category Indicator = N and Parent Caretaker Category Determination Date = current date and Parent Caretaker Category Ineligibility Reason = 146
+      context        
+    end
 
-    Business Rule Logic: Spouse or Domestic Partner of Caretaker Relative
-    1.  Caretaker Relationship – Spouse meets criteria  When Applicant Parent Caretaker Category Indicator = Y and Relationship Code(applicant to any other applicant) = 02 and (Spouse)Lives With (Applicant) = Y 
-    Then Applicant Parent Caretaker Category Indicator (for Spouse) = Y and Parent Caretaker Category Determination Date = current date and Parent Caretaker Category Ineligibility Reason = 999
+    # rule "Dependent Child Age Logic – Child under dependent age" do
+    #   for c_in, c_out in v("Applicant Child List").zip o["Qualified Children List"]
+    #     if c_in["Dependent Child Age"] < c("Dependent Age Threshold")
+    #       c_out["Child of Caretaker Dependent Age Indicator"] = 'Y' 
+    #       c_out["Child of Caretaker Dependent Age Determination Date"] = current_date
+    #       c_out["Child of Caretaker Dependent Age Ineligibility Reason"] = 999
+    #     end
+    #   end
+    # end
+    
+    # rule "Dependent Child Age Logic – Child older than dependent age" do
+    #   for c_in, c_out in v("Applicant Child List").zip o["Qualified Children List"]
+    #     if c_in["Dependent Child Age"] > c("Dependent Age Threshold")
+    #       c_out["Child of Caretaker Dependent Age Indicator"] = 'N'
+    #       c_out["Child of Caretaker Dependent Age Determination Date"] = current_date
+    #       c_out["Child of Caretaker Dependent Age Ineligibility Reason"] = 147
+    #     end
+    #   end
+    # end
+    
+    # rule "Dependent Child Age Logic – Child equal to dependent age and a student" do
+    #   for c_in, c_out in v("Applicant Child List").zip o["Qualified Children List"]
+    #     if c("Option Dependent Student") == 'Y' && c_in["Dependent Child Age"] == c("Dependent Age Threshold") && c_in["Student Indicator"] == 'Y'
+    #       c_out["Child of Caretaker Dependent Age Indicator"] = 'Y'
+    #       c_out["Child of Caretaker Dependent Age Determination Date"] = current_date
+    #       c_out["Child of Caretaker Dependent Age Ineligibility Reason"] = 999
+    #     end
+    #   end
+    # end
 
-    2.  Caretaker Relationship – Domestic Partner meets criteria  When Applicant Parent Caretaker Category Indicator = Y and Option Caretaker Relative Relationship = 02 or 03 and Relationship Code(applicant to any other applicant) = 08 and (Domestic Partner)Lives With (Applicant) = Y
-    Then Applicant Parent Caretaker Category Indicator (for Spouse) = Y and Parent Caretaker Category Determination Date = current date and Parent Caretaker Category Ineligibility Reason = 999
+    # 4.  Dependent Child Age Logic – Child is equal to dependent age but not a student When Option Dependent Student = Y and Dependent Child Age = Dependent Age Threshold and Student Indicator = N
+    # Then Child of Caretaker Dependent Age Indicator= N and Child of Caretaker Dependent Age Determination Date = current date and Child of Caretaker Dependent Age Ineligibility Reason = 137
+    # Continue to the Deprivation logic below with the current child record
+    # 5.  Dependent Deprived of Parental Support – Requirement not retained by state  When Deprivation Requirement Retained = N 
+    # Then Child of Caretaker Deprived Child Indicator =X and Child of Caretaker Deprived Child Determination Date = current date and  Child of Caretaker Deprived Child Ineligibility Reason = 555
+    # Continue to the Relationship logic below with the current child record
+
+    # 6.  Dependent Deprived of Parental Support – Requirement retained by state  When Deprivation Requirement Retained = Y 
+    # Then Child of Caretaker Deprived Child Indicator =T and Child of Caretaker Deprived Child Determination Date = current date and  Child of Caretaker Deprived Child Ineligibility Reason = 999
+    # Continue to the Relationship logic below with the current child record
+    # 7.  Caretaker Relationship – Caretaker meets standard definition  When Option Caretaker Relative Relationship = 00 and Relationship Code(caretaker to dependent child) = (03, 07, 12, 13, 14, 15, 16, 23)
+    # Then Child of Caretaker Relationship Indicator= Y and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 999
+    # Get next record from Applicant Children list and begin at Dependent Age logic.
+    # 8.  Caretaker Relationship – Caretaker does not meet standard definition  When Option Caretaker Relative Relationship = 00 and Relationship Code(caretaker to dependent child) <> (03, 07, 12, 13, 14, 15, 16, 23)
+    # Then Child of Caretaker Relationship Indicator= N and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 132
+    # Get next record from Applicant Children list and begin at Dependent Age logic.
+    # 9.  Caretaker Relationship – Caretaker meets any relative definition  When Option Caretaker Relative Relationship = 01 and Relationship Code(caretaker to dependent child) = (03, 07, 12, 13, 14, 15, 16, 23, 27, 30)
+    # Then Child of Caretaker Relationship Indicator= Y and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 999
+    # Get next record from Applicant Children list and begin at Dependent Age logic.
+    # 10. Caretaker Relationship – Caretaker does not meet any relative definition  When Option Caretaker Relative Relationship = 01 and Relationship Code(caretaker to dependent child) <>(03, 07, 12, 13, 14, 15, 16, 23, 27, 30)
+    # Then Child of Caretaker Relationship Indicator= N and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 131
+    # Get next record from Applicant Children list and begin at Dependent Age logic.
+    # 11. Caretaker Relationship – Any Adult – Caretaker is an adult  When Option Caretaker Relative Relationship = 04 and Applicant Age >= Child Age Threshold
+    # Then Child of Caretaker Relationship Indicator= Y and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 999
+    # Get next record from Applicant Children list and begin at Dependent Age logic.
+    # 12. Caretaker Relationship – Any Adult – Caretaker is not an Adult  When Option Caretaker Relative Relationship = 04 and Applicant Age < Child Age Threshold
+    # Then Child of Caretaker Relationship Indicator= N and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 130
+    # Get next record from Applicant Children list and begin at Dependent Age logic.
+    # 13. Caretaker Relationship – Domestic Partner – Individual is domestic partner  When Option Caretaker Relative Relationship = 02 and Relationship Code(to child) = 17
+    # Then Child of Caretaker Relationship Indicator= Y and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 999
+    # Get next record from Applicant Children list and begin at Dependent Age logic.
+    # 14. Caretaker Relationship – Domestic Partner Individual is not the domestic partner  When Option Caretaker Relative Relationship = 02 and Relationship Code(to child) <> 17
+    # Then Child of Caretaker Relationship Indicator= N and Child of Caretaker Relationship Determination Date = current date and Child of Caretaker Relationship Ineligibility Reason = 389
+    # Get next record from Applicant Children list and begin at Dependent Age logic.
+
+    # Business Rule Logic: Set Parent Caretaker Category
+    # 1.  Applicant Child list is empty – no children   When Applicant Children List is empty
+    # Then Applicant Parent Caretaker Category Indicator = N and Parent Caretaker Category Determination Date = current date and Parent Caretaker Category Ineligibility Reason = 146
+    # 2.  Child of Caretaker meets all criteria When Child of Caretaker Dependent Age Indicator= Y and Child of Caretaker Relationship Indicator= Y and Child of Caretaker Deprived Child Indicator = (X or T) 
+    # Add child record to the Qualified Children list and get next child record
+    # 3.  Child of Caretaker does not meet all criteria When Child of Caretaker Dependent Age Indicator= N or Child of Caretaker Deprived Child Indicator= N or Child of Caretaker Relationship Indicator= N
+    # Then get next child record 
+    # 4.  No more child records When Child of Caretaker Deprived Child Indicator=  T (for all records in Qualified Children list)
+    # Then Applicant Parent Caretaker Category Indicator = T and Parent Caretaker Category Determination Date = current date and Parent Caretaker Category Ineligibility Reason = 999 
+    # 5.  No more child records When Child of Caretaker Deprived Child Indicator <>  T (for all records in Qualified Children list)
+    # Then Applicant Parent Caretaker Category Indicator = Y and Parent Caretaker Category Determination Date = current date and Parent Caretaker Category Ineligibility Reason = 999 
+    # 6.  No more child records When Qualified Children List is empty
+    # Then Applicant Parent Caretaker Category Indicator = N and Parent Caretaker Category Determination Date = current date and Parent Caretaker Category Ineligibility Reason = 146
+
+    # Business Rule Logic: Spouse or Domestic Partner of Caretaker Relative
+    # 1.  Caretaker Relationship – Spouse meets criteria  When Applicant Parent Caretaker Category Indicator = Y and Relationship Code(applicant to any other applicant) = 02 and (Spouse)Lives With (Applicant) = Y 
+    # Then Applicant Parent Caretaker Category Indicator (for Spouse) = Y and Parent Caretaker Category Determination Date = current date and Parent Caretaker Category Ineligibility Reason = 999
+
+    # 2.  Caretaker Relationship – Domestic Partner meets criteria  When Applicant Parent Caretaker Category Indicator = Y and Option Caretaker Relative Relationship = 02 or 03 and Relationship Code(applicant to any other applicant) = 08 and (Domestic Partner)Lives With (Applicant) = Y
+    # Then Applicant Parent Caretaker Category Indicator (for Spouse) = Y and Parent Caretaker Category Determination Date = current date and Parent Caretaker Category Ineligibility Reason = 999
 
 
-    Constraints/ Special Instructions
-    1.  The Dependent Age Threshold is set as a variable for system configurability, not to support a state option. The value is set to 18.
+    # Constraints/ Special Instructions
+    # 1.  The Dependent Age Threshold is set as a variable for system configurability, not to support a state option. The value is set to 18.
 
   end
 end
