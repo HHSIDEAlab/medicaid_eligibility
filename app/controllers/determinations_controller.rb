98027841697b2acf18529aabec4398d12515f564
Curtis <curtismorales@gmail.com>
2013-07-26 20:47:03 -0400

    More determination skeleto

--- app/controllers/determinations_controller.rb
+++ app/controllers/determinations_controller.rb
@@ -1,5 +1,7 @@
 class DeterminationsController < ApplicationController
   def eval
-    render :xml => {}, :root => 'Response' 
+    determination = Determination.new(params['AccountTransferRequest'])
+
+    render :xml => determination.result, :root => 'Response' 
   end
 end
