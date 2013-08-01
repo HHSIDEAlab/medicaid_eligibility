22f0edc4ee4fb9f189e07e1b2c515f76be41dda5
Curtis <curtismorales@gmail.com>
2013-08-01 12:03:33 -0400

    Option to return the whole application or not

--- app/controllers/determinations_controller.rb
+++ app/controllers/determinations_controller.rb
@@ -1,6 +1,6 @@
 class DeterminationsController < ApplicationController
   def eval
-    app = Application.new(request.raw_post)
+    app = Application.new(request.raw_post, params[:return_application])
 
     render :xml => app.result
   end
