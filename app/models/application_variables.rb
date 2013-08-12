7a4cf1bb9bf8f1360bcf5cf5d2d8982eaf93c9fe
Curtis <curtismorales@gmail.com>
2013-08-12 18:41:11 -0400

    Add user-defined inputs

--- app/models/application_variables.rb
+++ app/models/application_variables.rb
@@ -3,8 +3,9 @@ module ApplicationVariables
     {
       :name       => "Applicant Age",
       :type       => :integer,
-      :xml_group  => :undefined,
-      :xpath      => :undefined
+      :xml_group  => :person,
+      :xpath      => "PersonAge",
+      :new_variable => true
     },
     {
       :name       => "Applicant Attest Disabled",
@@ -20,12 +21,12 @@ module ApplicationVariables
       :xml_group  => :applicant,
       :xpath      => "hix-ee:InsuranceApplicantBlindnessOrDisabilityIndicator"
     },
-    {
-      :name       => "Applicant Household Income",
-      :type       => :integer,
-      :xml_group  => :undefined,
-      :xpath      => :undefined
-    },
+    # {
+    #   :name       => "Applicant Household Income",
+    #   :type       => :integer,
+    #   :xml_group  => :undefined,
+    #   :xpath      => :undefined
+    # },
     {
       :name       => "Applicant Medicaid Citizen Or Immigrant Status Indicator",
       :type       => :flag,
@@ -40,13 +41,14 @@ module ApplicationVariables
       :xml_group  => :person,
       :xpath      => "hix-core:PersonAugmentation/hix-core:PersonPregnancyStatus/hix-core:StatusIndicator"
     },
-    # {
-    #   :name       => "Applicant Post Partum Period Indicator",
-    #   :type       => :flag,
-    #   :values     => %w(Y N),
-    #   :xml_group  => :person,
-    #   :xpath      => "hix-core:PersonAugmentation/hix-core:PersonPostPartumPeriod/hix-core:StatusIndicator"
-    # },
+    {
+      :name       => "Applicant Post Partum Period Indicator",
+      :type       => :flag,
+      :values     => %w(Y N),
+      :xml_group  => :person,
+      :xpath      => "hix-core:PersonAugmentation/hix-core:PersonPostPartumPeriod/hix-core:StatusIndicator",
+      :new_variable => true
+    },
     {
       :name       => "Medicare Entitlement Indicator",
       :type       => :flag,
@@ -66,7 +68,7 @@ module ApplicationVariables
       :type       => :flag,
       :values     => %w(Y N),
       :xml_group  => :applicant,
-      :xpath      => "hix-ee:MedicaidNonMAGIEligibility/hix-ee:MedicaidNonMAGIBlindnessOrDisabilityEligibilityBasis/hix-ee:EligibilityBasisStatusIndicator"
+      :xpath      => "hix-ee:MedicaidNonMAGIEligibility/hix-ee:MedicaidNonMAGIBlindnessOrDisabilityEligibilityBasis/hix-core:StatusIndicator"
     }
   ].freeze
 
@@ -94,4 +96,25 @@ module ApplicationVariables
       :xpath  => :undefined
     }
   ].freeze
+
+  RELATIONSHIP_CODES = {
+    "01" => "Self",
+    "02" => "Spouse of other relative within required degree of relation",
+    "03" => "Parent",
+    "04" => "Son/Daughter",
+    "05" => "Stepdaughter/Stepson",
+    "06" => "Grandchild",
+    "07" => "Sibling/Stepsibling",
+    "08" => "Domestic Partner of other relative within required degree of relation",
+    "12" => "Step-Parent",
+    "13" => "Uncle/Aunt",
+    "14" => "Nephew/Niece",
+    "15" => "Grandparent",
+    "16" => "First Cousin",
+    "17" => "Parent's Domestic Partner",
+    "23" => "Brother-in-Law/Sister-in-Law",
+    "26" => "Daughter-in-Law/Son-in-Law",
+    "27" => "Former Spouse",
+    "30" => "Mother-in-Law/Father-in-Law"
+  }.freeze
 end
