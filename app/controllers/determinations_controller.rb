91620ba11b7a1d0e4886a6190af7f3b8a451f908
Curtis <curtismorales@gmail.com>
2013-09-01 18:05:54 -0400

    Improved controller (still not quite right)

--- app/controllers/determinations_controller.rb
+++ app/controllers/determinations_controller.rb
@@ -1,15 +1,12 @@
 class DeterminationsController < ApplicationController
-  def eval
-    return_format = request.accept
-    content_type = request.content_type
-    app = Application.new(request.raw_post, request.content_type, params[:return_application])
+  include ActionController::MimeResponds
 
-    if return_format == 'application/xml'
-      render :xml => app.result(:xml)
-    elsif return_format == 'application/json'
-      render :json => JSON.pretty_generate(app.result(:json))
-    else
-      render 'Specify a return format'
+  def eval
+    @app = Application.new(request)
+
+    respond_to do |format|
+      format.xml { render xml: @app }
+      format.json { render json: @app }
     end
   end
 end
