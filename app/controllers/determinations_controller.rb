740b2d3dbeeb3259404185eacb5d5847771d0143
Erek Dyskant <erek.dyskant@bluelabs.com>
2013-09-04 16:37:31 -0400

    Added form. Added configuration files to version control

--- app/controllers/determinations_controller.rb
+++ app/controllers/determinations_controller.rb
@@ -2,8 +2,14 @@ class DeterminationsController < ApplicationController
   def eval
     return_format = request.accept
     content_type = request.content_type
-    app = Application.new(request.raw_post, request.content_type, params[:return_application])
-
+    if(params.has_key?(:json_request)) 
+       return_format = 'application/json'
+       print "test"
+       app = Application.new(params[:json_request], 'application/json', params[:return_application])
+    else
+       app = Application.new(request.raw_post , request.content_type, params[:return_application])
+    end
+    
     if return_format == 'application/xml'
       render :xml => app.result(:xml)
     elsif return_format == 'application/json'
@@ -12,4 +18,10 @@ class DeterminationsController < ApplicationController
       render 'Specify a return format'
     end
   end
+
+  def json_form_post
+    request.accept = 'application/json'
+    eval
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 MedicaidEligibilityApi::Application.routes.draw do
+  
   resources :determinations, defaults: {format: 'xml'}, only: [] do
     post 'eval', :on => :collection
   end
@@ -6,4 +7,6 @@ MedicaidEligibilityApi::Application.routes.draw do
   resources :rulesets, defaults: {format: 'json'}, constraints: { :id => /.+?/ } do
     post 'eval', :on => :member
   end
+  
+
 end
