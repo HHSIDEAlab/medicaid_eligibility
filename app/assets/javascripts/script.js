32816b6818b2cbae994d1870097dc0d1b16c4d8b
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2013-09-05 17:09:10 -0700

    Adding Uniform

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -10,9 +10,6 @@ var resetForm = function ($form) {
         $applicantRelationshipFields = $('.applicant-relationship-fields', el),
         relationshipTemplate = _.template($('#relationship_template').html());
       $('.applicant-number', el).text(i + 1);
-      while ($applicantRelationshipFields.find('form-row').length > i) {
-
-      }
       for (rI = 0; rI < i; rI += 1) {
         if ($applicantRelationshipFields.find('.form-row').length < i) {
           $applicantRelationshipFields
@@ -30,29 +27,38 @@ var resetForm = function ($form) {
       });
       $(el).toggleClass('last-applicant', i === $fieldsets.length - 1)
     });
+  },
+  uniform = function () {
+    $('input[type=checkbox], select').uniform({selectAutoWidth: false});
   };
 
 $(document).on('change', '[type=checkbox]', function () {
   var $this = $(this),
     checked = $(this).is(':checked'),
-    $parent = $this.parent('.form-row-expandable'),
+    $parent = $this.closest('.checker').parent('.form-row-expandable'),
     exclusive = $this.data('exclusive');
   $parent.toggleClass('form-row-expanded', checked);
   $parent.find('.form-row-expandable-fields').slideToggle(checked);
   if (exclusive) {
-    $this.closest('fieldset').find('input[data-exclusive=' + exclusive + ']').not($this)
-      .attr('checked', !checked);
+    $this.closest('fieldset').find('input[data-exclusive=' + exclusive + ']:checked').not($this)
+      .attr('checked', false);
   }
+  $.uniform.update();
 }).on('click', '.add-applicant', function () {
   var $fieldsets = $('fieldset'),
-    $newFieldset = $fieldsets.first().clone();
+    $newFieldset;
+  $.uniform.restore('input[type=checkbox], select');
+  $newFieldset = $fieldsets.first().clone();
   $newFieldset.removeClass('first-applicant');
   resetForm($newFieldset);
   $fieldsets.last().after($newFieldset);
   countFieldsets();
+  uniform();
 }).on('click', '.remove-applicant', function () {
   if ($('fieldset').length > 1) {
     $(this).closest('fieldset').remove();
     countFieldsets();
   }
 });
+
+$(uniform);
