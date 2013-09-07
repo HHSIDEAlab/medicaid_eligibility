40f6975d68c5a0a7ca2187e4c71719bd528287d1
Joe Arasin <joe@arasin.com>
2013-09-07 14:47:29 -0400

    Don't fetch test json

--- app/assets/javascripts/results.js
+++ app/assets/javascripts/results.js
@@ -17,8 +17,3 @@ function showPerson(event){
 	var template = $("#person_template").html();
 	$("#person").html(_.template(template,{person: details}));
 };
-
-$.getJSON("/test.json",function(results){
-	Results = results;
-	refreshResults();
-});
