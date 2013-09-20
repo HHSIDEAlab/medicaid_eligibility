fbad388171a6294bbf6952e524170b076602546e
Joe Arasin <joe@arasin.com>
2013-09-20 19:07:25 -0400

    Major code cleanup

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,172 +1,36 @@
 'use strict';
 
 angular.module('MAGI.controllers', []).
-	controller('FormController',['$scope','filterFilter', function($scope,filterFilter){		
-		$scope.application = {
-			applicants: [],
-			taxReturns: []
-		};
+	controller('FormController',['$scope','filterFilter', 'Application','relationshipCodes','states', function($scope,filterFilter, Application, relationshipCodes,states){		
+                $scope.applicants = Application.applicants;
+                $scope.taxReturns = Application.taxReturns;
+                $scope.state = Application.state;
 
-                $scope.$watch('application.applicants.length', function (newValue, oldValue){
-                        $scope.numFilers = Math.min(newValue,2);
-                        $scope.numDependents = Math.max(newValue-1,0);
-
-                        angular.forEach($scope.application.taxReturns, function(taxReturn){
-                                $scope.checkNumFilers(taxReturn);
-                        });
-
-                },true);
-
-		$scope.states = [
-			{abbr: 'AL', name: 'Alabama'},
-                        {abbr: 'AK', name: 'Alaska'},
-                        {abbr: 'AZ', name: 'Arizona'},
-                        {abbr: 'AR', name: 'Arkansas'},
-                        {abbr: 'CA', name: 'California'},
-                        {abbr: 'CO', name: 'Colorado'},
-                        {abbr: 'CT', name: 'Connecticut'},
-                        {abbr: 'DE', name: 'Delaware'},
-                        {abbr: 'DC', name: 'District Of Columbia'},
-                        {abbr: 'FL', name: 'Florida'},
-                        {abbr: 'GA', name: 'Georgia'},
-                        {abbr: 'HI', name: 'Hawaii'},
-                        {abbr: 'ID', name: 'Idaho'},
-                        {abbr: 'IL', name: 'Illinois'},
-                        {abbr: 'IN', name: 'Indiana'},
-                        {abbr: 'IA', name: 'Iowa'},
-                        {abbr: 'KS', name: 'Kansas'},
-                        {abbr: 'KY', name: 'Kentucky'},
-                        {abbr: 'LA', name: 'Louisiana'},
-                        {abbr: 'ME', name: 'Maine'},
-                        {abbr: 'MD', name: 'Maryland'},
-                        {abbr: 'MA', name: 'Massachusetts'},
-                        {abbr: 'MI', name: 'Michigan'},
-                        {abbr: 'MN', name: 'Minnesota'},
-                        {abbr: 'MS', name: 'Mississippi'},
-                        {abbr: 'MO', name: 'Missouri'},
-                        {abbr: 'MT', name: 'Montana'},
-                        {abbr: 'NE', name: 'Nebraska'},
-                        {abbr: 'NV', name: 'Nevada'},
-                        {abbr: 'NH', name: 'New Hampshire'},
-                        {abbr: 'NJ', name: 'New Jersey'},
-                        {abbr: 'NM', name: 'New Mexico'},
-                        {abbr: 'NY', name: 'New York'},
-                        {abbr: 'NC', name: 'North Carolina'},
-                        {abbr: 'ND', name: 'North Dakota'},
-                        {abbr: 'OH', name: 'Ohio'},
-                        {abbr: 'OK', name: 'Oklahoma'},
-                        {abbr: 'OR', name: 'Oregon'},
-                        {abbr: 'PA', name: 'Pennsylvania'},
-                        {abbr: 'RI', name: 'Rhode Island'},
-                        {abbr: 'SC', name: 'South Carolina'},
-                        {abbr: 'SD', name: 'South Dakota'},
-                        {abbr: 'TN', name: 'Tennessee'},
-                        {abbr: 'TX', name: 'Texas'},
-                        {abbr: 'UT', name: 'Utah'},
-                        {abbr: 'VT', name: 'Vermont'},
-                        {abbr: 'VA', name: 'Virginia'},
-                        {abbr: 'WA', name: 'Washington'},
-                        {abbr: 'WV', name: 'West Virginia'},
-                        {abbr: 'WI', name: 'Wisconsin'},
-                        {abbr: 'WY', name: 'Wyoming'},
-		];
-
-                $scope.relationshipCodes = [
-                        {code: '02', label: 'Spouse', opposite: '02'},
-                        {code: '03', label: 'Parent', opposite: '04'},
-                        {code: '04', label: 'Child', opposite: '03'},
-                        {code: '05', label: 'Stepchild', opposite: '12'},
-                        {code: '06', label: 'Grandchild', opposite: '15'},
-                        {code: '07', label: 'Sibling', opposite: '07'},
-                        {code: '08', label: 'Domestic partner', opposite: '08'},
-                        {code: '12', label: 'Stepparent', opposite: '05'},
-                        {code: '13', label: 'Uncle/Aunt', opposite: '14'},
-                        {code: '14', label: 'Nephew/Niece', opposite: '13'},
-                        {code: '15', label: 'Grandparent', opposite: '06'},
-                        {code: '16', label: 'Cousin', opposite: '16'},
-                        {code: '17', label: "Parent's domestic partner", opposite: 'XX'},
-                        {code: '23', label: 'Sibling-in-law', opposite: '23'},
-                        {code: '26', label: 'Child-in-law', opposite: '30'},
-                        {code: '27', label: 'Former spouse', opposite: '27'},
-                        {code: '30', label: 'Parent-in-law', opposite: '26'},
-                        {code: 'XX', label: "Domestic partner's child", opposite: '17'},
-                        {code: '88', label: "Other", opposite: '88'}
-                ]
+                $scope.addTaxReturn = Application.addTaxReturn;
+                $scope.removeApplicant = Application.removeApplicant;
+                $scope.removeTaxReturn = Application.removeTaxReturn;
+                $scope.relationshipCodes = relationshipCodes;
 
 
-		$scope.addApplicant = function(){
-                        var newApplicant = {
-                                id: "Applicant " + ($scope.application.applicants.length+1),
-                                citizen: true,
-                                stateResidency: true,
-                                isApplicant: true,
-                                relationships: _.map($scope.application.applicants, function(applicant){
-                                        return {otherApplicant: applicant};
-                                })
-                        }
+                $scope.addApplicant = function(){
+                        Application.addApplicant("Applicant " + ($scope.applicants.length+1));
+                };
 
-			angular.forEach($scope.application.applicants,function(applicant){
-                                applicant.relationships.push({otherApplicant: newApplicant});
-                        });
-
-                        $scope.application.applicants.push(newApplicant);
-                        
-		}
-
-		$scope.addTaxReturn = function(){
-                        var taxReturn = {
-                                filers:[],
-                                dependents: []
-                        };
-                        $scope.checkNumFilers(taxReturn);
-			$scope.application.taxReturns.push(taxReturn);
-		}
-
-                $scope.checkNumFilers = function(taxReturn){
-                        while(taxReturn.filers.length < $scope.numFilers){
-                                taxReturn.filers.push({});
-                        }
-                        while(taxReturn.filers.length > $scope.numFilers){
-                                taxReturn.filers.pop();
-                        }
-                        while(taxReturn.dependents.length < $scope.numDependents){
-                                taxReturn.dependents.push({});
-                        }
-                        while(taxReturn.dependents.length > $scope.numDependents){
-                                taxReturn.dependents.pop();
-                        }
+                $scope.showFilers = function(){
+                        return $scope.applicants.length > 0;
                 }
 
-                $scope.removeApplicant = function(applicant){
-                        var removeIndex = $scope.application.applicants.indexOf(applicant);
-                        $scope.application.applicants.splice(removeIndex,1);
-                        angular.forEach($scope.application.applicants,function(otherApplicant){
-                                var otherRel = _.find(otherApplicant.relationships, function(rel){
-                                        return rel.otherApplicant == applicant;
-                                });
-                                var otherRelIndex = otherApplicant.relationships.indexOf(otherRel);
-                                otherApplicant.relationships.splice(otherRelIndex, 1);
-                        });
-
-                        if($scope.application.applicants.length == 0){
-                                $scope.addApplicant();
-                        }
-                }
-
-                $scope.removeTaxReturn = function(taxReturn){
-                        $scope.application.taxReturns.splice($scope.application.taxReturns.indexOf(taxReturn),1);
-                        if($scope.application.taxReturns.length == 0){
-                                $scope.addTaxReturn();
-                        }
+                $scope.showDependents = function(){
+                        return $scope.applicants.length > 1;
                 }
 
 
-		$scope.doStuff = function(){
-			$scope.application.applicants[0].pregnant = true;
-		}
-
+                // We want to initialize the application with an applicant and a tax return
                 $scope.addApplicant();
                 $scope.addTaxReturn();
+
+
+		$scope.states = states;
 	}]).
         controller('ApplicantController',['$scope',function($scope){
                 $scope.checkResponsibility = function(){
@@ -177,16 +41,22 @@ angular.module('MAGI.controllers', []).
                         $scope.applicant.nonParentResponsibility = false;
                 });
 
+                $scope.$watch('applicant.pregnantThreeMonths', function(newValue,oldValue){
+                        if(newValue){
+                                $scope.applicant.pregnant = false;
+                        }
+                });
+
+                $scope.$watch('applicant.pregnant', function(newValue,oldValue){
+                        if(newValue){
+                                $scope.applicant.pregnantThreeMonths = false;
+                        }
+                });
+
+                        
+
                 $scope.updateRelationship = function(relationship){
-                        var otherRel = _.find(relationship.otherApplicant.relationships,function(rel){
-                                return rel.otherApplicant==$scope.applicant;
-                        });
-
-                        var myCode = _.find($scope.relationshipCodes, function(rc){
-                                return rc.code == relationship.code;
-                        });
-
-                        otherRel.code = myCode.opposite;
+                        relationship.updateOpposite();
                 }
 
                 $scope.notMe = function(other) {
--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,1 +1,198 @@
-angular.module('MAGI.services',[]);
+angular.module('MAGI.services',[]).
+	factory('Application', function(relationshipCodes){
+		function Application(){
+			this.applicants = [];
+			this.taxReturns = [];
+		}
+
+		function TaxReturn(){
+			this.filers = [];
+			this.dependents = [];
+		}
+
+		function Applicant(){
+			this.citizen = true;
+			this.stateResidency = true;
+			this.isApplicant = true;
+			this.relationships = [];
+		}
+
+		function Relationship(applicant, otherApplicant, code){
+			this.applicant = applicant;
+			this.otherApplicant = otherApplicant;
+			this.code = code;
+		}
+
+		// Note. Remove this eventually. UI should not have to deal with service
+		// For now, sets the number of filers and dependents in a tax return to 
+		// be an appropriate amount for the number of applicants
+		TaxReturn.prototype.checkNumFilers = function(numApplicants){
+			var numFilers = Math.min(numApplicants,2);
+			while(this.filers.length < numFilers){
+                    this.filers.push({});
+            }
+            while(this.filers.length > numFilers){
+                    this.filers.pop();
+            }
+            
+            var numDependents = Math.max(numApplicants-1,0);
+            while(this.dependents.length < numDependents){
+                    this.dependents.push({});
+            }
+            while(this.dependents.length > numDependents){
+                    this.dependents.pop();
+            }
+		}
+
+		Application.prototype.addTaxReturn = function(){
+			var taxReturn = new TaxReturn();
+			taxReturn.checkNumFilers(this.applicants.length);
+			this.taxReturns.push(taxReturn);
+		}
+
+		Application.prototype.removeTaxReturn = function(taxReturn){
+			this.taxReturns.splice(this.taxReturns.indexOf(taxReturn),1);
+            if(this.taxReturns.length == 0){
+            	this.addTaxReturn();
+            }
+		}
+
+		Application.prototype.addApplicant = function(applicantName){
+			var applicant = new Applicant();
+			applicant.id = applicantName;
+
+			angular.forEach(this.applicants, function(other){
+				other.addRelationship(applicant);
+				applicant.addRelationship(other);
+			});
+
+			this.applicants.push(applicant);
+			var appl = this;
+
+			angular.forEach(this.taxReturns, function(taxReturn){
+				taxReturn.checkNumFilers(appl.applicants.length)
+			});
+		}
+
+		Applicant.prototype.addRelationship = function(other){
+			this.relationships.push(new Relationship(this, other, ''));
+		};
+
+		Applicant.prototype.getRelationship = function(otherApplicant){
+			return _.find(this.relationships, function(rel){
+				return rel.otherApplicant == otherApplicant;
+			});
+		}
+
+		Applicant.prototype.removeRelationship = function(otherApplicant){
+			var toRemove = this.getRelationship(otherApplicant);
+			var idx = this.relationships.indexOf(toRemove);
+            this.relationships.splice(idx, 1);
+		}
+
+		Applicant.prototype.removeRelationships = function(){
+			angular.forEach(this.relationships, function(rel){
+				rel.otherApplicant.removeRelationship(this);
+			});
+		}
+
+		Application.prototype.removeApplicant = function(applicant){
+			applicant.removeRelationships();
+			var removeIndex = this.applicants.indexOf(applicant);
+            this.applicants.splice(removeIndex,1);
+            
+            if( this.applicants.length == 0 ){
+                        	this.addApplicant();
+            }
+		}
+
+		Relationship.prototype.getOpposite = function(){
+			return this.otherApplicant.getRelationship(this.applicant);
+		}
+
+		Relationship.prototype.updateOpposite = function(){
+			var me = this;
+			this.getOpposite().code = _.filter(relationshipCodes, function(rc){
+                    return rc.code == me.code;
+            });
+		}
+
+		return new Application();
+
+	})
+	.constant('relationshipCodes',
+		[
+            {code: '02', label: 'Spouse', opposite: '02'},
+            {code: '03', label: 'Parent', opposite: '04'},
+            {code: '04', label: 'Child', opposite: '03'},
+            {code: '05', label: 'Stepchild', opposite: '12'},
+            {code: '06', label: 'Grandchild', opposite: '15'},
+            {code: '07', label: 'Sibling', opposite: '07'},
+            {code: '08', label: 'Domestic partner', opposite: '08'},
+            {code: '12', label: 'Stepparent', opposite: '05'},
+            {code: '13', label: 'Uncle/Aunt', opposite: '14'},
+            {code: '14', label: 'Nephew/Niece', opposite: '13'},
+            {code: '15', label: 'Grandparent', opposite: '06'},
+            {code: '16', label: 'Cousin', opposite: '16'},
+            {code: '17', label: "Parent's domestic partner", opposite: 'XX'},
+            {code: '23', label: 'Sibling-in-law', opposite: '23'},
+            {code: '26', label: 'Child-in-law', opposite: '30'},
+            {code: '27', label: 'Former spouse', opposite: '27'},
+            {code: '30', label: 'Parent-in-law', opposite: '26'},
+            {code: 'XX', label: "Domestic partner's child", opposite: '17'},
+            {code: '88', label: "Other", opposite: '88'}
+        ]
+	).constant('states',[
+			{abbr: 'AL', name: 'Alabama'},
+            {abbr: 'AK', name: 'Alaska'},
+            {abbr: 'AZ', name: 'Arizona'},
+            {abbr: 'AR', name: 'Arkansas'},
+            {abbr: 'CA', name: 'California'},
+            {abbr: 'CO', name: 'Colorado'},
+            {abbr: 'CT', name: 'Connecticut'},
+            {abbr: 'DE', name: 'Delaware'},
+            {abbr: 'DC', name: 'District Of Columbia'},
+            {abbr: 'FL', name: 'Florida'},
+            {abbr: 'GA', name: 'Georgia'},
+            {abbr: 'HI', name: 'Hawaii'},
+            {abbr: 'ID', name: 'Idaho'},
+            {abbr: 'IL', name: 'Illinois'},
+            {abbr: 'IN', name: 'Indiana'},
+            {abbr: 'IA', name: 'Iowa'},
+            {abbr: 'KS', name: 'Kansas'},
+            {abbr: 'KY', name: 'Kentucky'},
+            {abbr: 'LA', name: 'Louisiana'},
+            {abbr: 'ME', name: 'Maine'},
+            {abbr: 'MD', name: 'Maryland'},
+            {abbr: 'MA', name: 'Massachusetts'},
+            {abbr: 'MI', name: 'Michigan'},
+            {abbr: 'MN', name: 'Minnesota'},
+            {abbr: 'MS', name: 'Mississippi'},
+            {abbr: 'MO', name: 'Missouri'},
+            {abbr: 'MT', name: 'Montana'},
+            {abbr: 'NE', name: 'Nebraska'},
+            {abbr: 'NV', name: 'Nevada'},
+            {abbr: 'NH', name: 'New Hampshire'},
+            {abbr: 'NJ', name: 'New Jersey'},
+            {abbr: 'NM', name: 'New Mexico'},
+            {abbr: 'NY', name: 'New York'},
+            {abbr: 'NC', name: 'North Carolina'},
+            {abbr: 'ND', name: 'North Dakota'},
+            {abbr: 'OH', name: 'Ohio'},
+            {abbr: 'OK', name: 'Oklahoma'},
+            {abbr: 'OR', name: 'Oregon'},
+            {abbr: 'PA', name: 'Pennsylvania'},
+            {abbr: 'RI', name: 'Rhode Island'},
+            {abbr: 'SC', name: 'South Carolina'},
+            {abbr: 'SD', name: 'South Dakota'},
+            {abbr: 'TN', name: 'Tennessee'},
+            {abbr: 'TX', name: 'Texas'},
+            {abbr: 'UT', name: 'Utah'},
+            {abbr: 'VT', name: 'Vermont'},
+            {abbr: 'VA', name: 'Virginia'},
+            {abbr: 'WA', name: 'Washington'},
+            {abbr: 'WV', name: 'West Virginia'},
+            {abbr: 'WI', name: 'Wisconsin'},
+            {abbr: 'WY', name: 'Wyoming'},
+		]
+	);
