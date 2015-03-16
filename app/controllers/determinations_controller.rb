5bea00a5939051c7b84b2ba599661d29c380d44f
Curtis Morales <curtismorales@gmail.com>
2015-03-16 14:45:53 -0400

    Make access token an option

--- app/controllers/determinations_controller.rb
+++ app/controllers/determinations_controller.rb
@@ -19,7 +19,11 @@ class DeterminationsController < ApplicationController
 
   def restrict_access
     authenticate_or_request_with_http_token do |token, options|
-      @@access_tokens.include? token
+      if ENV['REQUIRE_ACCESS_TOKEN'] == 'true'
+        @@access_tokens.include? token
+      else
+        true
+      end
     end
   end
 end
