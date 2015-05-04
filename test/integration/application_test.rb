d23542931d68561531d108fc30b95447df90d9b9
colinxfleming <c3flemin@gmail.com>
2015-05-04 14:56:04 -0400

    get json fixture in

--- test/integration/application_test.rb
+++ test/integration/application_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class ApplicationTest < ActionDispatch::IntegrationTest
 	def setup
+		@json = JSON.parse(File.read(Rails.root.to_s + '/test/fixtures/individual.json'))
 	end
 
   test "the truth" do
