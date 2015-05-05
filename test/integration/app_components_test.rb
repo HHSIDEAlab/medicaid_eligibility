ae4f64b74de573ae24694e858b5f00a1acc87ee0
colinxfleming <c3flemin@gmail.com>
2015-05-05 12:53:06 -0400

    create app components test

---
+++ test/integration/app_components_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+# mostly just object safety check testing of Applicant, Relationship, MedicaidHousehold, and TaxReturn
+
+class AppComponentsTest < ActionDispatch::IntegrationTest
+	include ApplicationComponents
+
+	def setup
+		@billy = Applicant.new 1, "handsome", "Billy Everyteen", "Not Important", 40000
+		@johnny = Applicant.new 2, "tightlipped", "Johnny Tightlips", "Not Important", 40000
+	end
+
+	test 'applicants should be able to get relationships' do
+		assert @billy.respond_to? :get_relationships
+		assert @billy.respond_to? :get_relationship
+	end
+end
