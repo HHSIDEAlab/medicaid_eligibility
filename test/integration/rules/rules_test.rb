6436332e5a76ace61efc5f272c4ac8b32c23b2ce
colinxfleming <c3flemin@gmail.com>
2015-07-02 10:50:03 -0400

    adjust to take everything in the fixtures directory; metaprogram the tests

--- test/integration/rules/rules_test.rb
+++ test/integration/rules/rules_test.rb
@@ -7,25 +7,36 @@ class MagiRulesTest < ActionDispatch::IntegrationTest
 	include ApplicationProcessor
 	include ApplicationParser
 
-	@fixture = AdultGroupFixture.new
+	# dynamically load and transform names of everything in fixtures/rule_fixtures
+	@all_fixtures = Dir.entries(Rails.root + 'test/fixtures/rule_fixtures').select { |file| file.to_s.length > 2 }.map do |file|
+		file.gsub('.rb','').split('_').map { |x| /chip/i.match(x) ? x.upcase : x.capitalize }.join('')
+	end
 
-	@fixture.test_sets.each do |set|
-		test "#{@fixture.magi} - #{set[:test_name]}" do 
-			if set[:test_name] =~ /Bad Info/
-				assert_raises RuleContext::MissingVariableError do 
+	@all_fixtures.each do |fixture| 
+		# generate objects from all_fixtures 
+		magi_fixture = eval "#{fixture}Fixture.new"
+
+		# generate tests based on fixture.test_sets
+		magi_fixture.test_sets.each do |set|
+			test "#{magi_fixture.magi} - #{set[:test_name]}" do 
+				# should raise a MissingVariableError if it's missing an input
+				if set[:test_name] =~ /Bad Info/
+					assert_raises RuleContext::MissingVariableError do 
+						context = RuleContext.new set[:configs], set[:inputs], Time.now.yesterday
+						result = eval "MAGI::#{fixture}.new.run context"
+
+						set[:expected_outputs].each_key do |out|
+							assert_equal result.output[out], set[:expected_outputs][out]
+						end
+					end
+				# should run clean
+				else
 					context = RuleContext.new set[:configs], set[:inputs], Time.now.yesterday
-					@result = MAGI::AdultGroup.new.run context
+					result = eval "MAGI::#{fixture}.new.run context"
 
 					set[:expected_outputs].each_key do |out|
-						assert_equal @result.output[out], set[:expected_outputs][out]
+						assert_equal result.output[out], set[:expected_outputs][out]
 					end
-				end
-			else
-				context = RuleContext.new set[:configs], set[:inputs], Time.now.yesterday
-				@result = MAGI::AdultGroup.new.run context
-
-				set[:expected_outputs].each_key do |out|
-					assert_equal @result.output[out], set[:expected_outputs][out]
 				end
 			end
 		end
