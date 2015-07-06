ee6523df2c211d9241d7ec86dbadc14b906b994f
colinxfleming <c3flemin@gmail.com>
2015-07-06 11:18:11 -0400

    add rule fixture for chip waiting period

---
+++ test/fixtures/rule_fixtures/chip_waiting_period.rb
@@ -0,0 +1,74 @@
+##### NOT CONFIRMED ######
+class CHIPWaitingPeriodFixture < MagiFixture
+	attr_accessor :magi, :test_sets
+
+	def initialize
+		super
+		@magi = 'CHIPWaitingPeriod'
+		@test_sets = [
+			{
+				test_name: "Determine CHIP Waiting Period - Waiting Period Satisfied",
+				inputs: { 
+					"Applicant CHIP Prelim Indicator" => "Y",
+					"Prior Insurance" => "Y",
+					"Prior Insurance End Date" => 2.months.ago
+				},
+				configs: {
+					"State CHIP Waiting Period" => 1
+					# should calculate a state chip wedding period end date of: 1 month ago
+				},
+				expected_outputs: {
+					"Applicant CHIP Waiting Period Satisfied Indicator" => "Y",
+					"CHIP Waiting Period Satisfied Ineligibility Reason" => 999
+				}
+			},
+			{
+				test_name: "Determine CHIP Waiting Period - Waiting Period Not Satisfied - Prior Insurance End Date",
+				inputs: { 
+					"Applicant CHIP Prelim Indicator" => "Y",
+					"Prior Insurance" => "Y",
+					"Prior Insurance End Date" => 2.months.ago
+				},
+				configs: {
+					"State CHIP Waiting Period" => 5
+					# should calculate a state chip wedding period end date of: 3 months from now
+				},
+				expected_outputs: {
+					"Applicant CHIP Waiting Period Satisfied Indicator" => "N",
+					"CHIP Waiting Period Satisfied Ineligibility Reason" => 139
+				}
+			},
+			{
+				test_name: "Determine CHIP Waiting Period - No Waiting Period - Required Input is 0",
+				inputs: { 
+					"Applicant CHIP Prelim Indicator" => "N",
+					"Prior Insurance" => "Y",
+					"Prior Insurance End Date" => 2.months.ago
+				},
+				configs: {
+					"State CHIP Waiting Period" => 0
+				},
+				expected_outputs: {
+					"Applicant CHIP Waiting Period Satisfied Indicator" => "X",
+					"CHIP Waiting Period Satisfied Ineligibility Reason" => 555
+				}
+			},
+			{
+				test_name: "Determine CHIP Waiting Period - No Waiting Period - No Prior Insurance",
+				inputs: { 
+					"Applicant CHIP Prelim Indicator" => "N",
+					"Prior Insurance" => "N",
+					"Prior Insurance End Date" => nil
+				},
+				configs: {
+					"State CHIP Waiting Period" => 0
+				},
+				expected_outputs: {
+					"Applicant CHIP Waiting Period Satisfied Indicator" => "X",
+					"CHIP Waiting Period Satisfied Ineligibility Reason" => 555
+				}
+			},
+
+		]
+	end
+end
