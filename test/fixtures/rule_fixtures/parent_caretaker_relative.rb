302bff686ffa0a41e05609d7eb032f46da42b363
colinxfleming <c3flemin@gmail.com>
2015-07-07 11:18:09 -0400

    add stubs for parent caretaker relative X and complete fixture for pregnant

---
+++ test/fixtures/rule_fixtures/parent_caretaker_relative.rb
@@ -0,0 +1,40 @@
+##### GENERATED AT 2015-07-07 11:06:11 -0400 ######
+class ParentCaretakerRelativeFixture < MagiFixture
+	attr_accessor :magi, :test_sets
+
+	def initialize
+		super
+		@magi = 'ParentCaretakerRelative'
+		@test_sets = [
+			# {
+			# 	test_name: "FILL_IN_WITH_TEST_NAME",
+			# 	inputs: {
+			# 		"Person ID" => ?,
+			# 		"Person List" => ?,
+			# 		"Physical Household" => ?,
+			# 		"Tax Returns" => ?,
+			# 		"Applicant Age" => ?,
+			# 		"Applicant Relationships" => ?
+			# 	},
+			# 	configs: {
+			# 		"Child Age Threshold" => ?,
+			# 		"Dependent Age Threshold" => ?,
+			# 		"Option Dependent Student" => ?,
+			# 		"Deprivation Requirement Retained" => ?,
+			# 		"Option Caretaker Relative Relationship" => ?,
+			# 		"State Unemployed Standard" => ?
+			# 	},
+			# 	expected_outputs: {
+			# 		"Applicant Parent Caretaker Category Indicator" => ?,
+			# 		"Parent Caretaker Category Determination Date" => ?,
+			# 		"Parent Caretaker Category Ineligibility Reason" => ?,
+			# 		"Qualified Children List" => ?
+			# 	}
+			# }
+		]
+	end
+end
+
+
+# NOTES
+# leaving this for later because it's super complicated. -CF 7/7/2015
---
+++ test/fixtures/rule_fixtures/parent_caretaker_relative_spouse.rb
@@ -0,0 +1,33 @@
+##### GENERATED AT 2015-07-07 11:07:02 -0400 ######
+class ParentCaretakerRelativeSpouseFixture < MagiFixture
+	attr_accessor :magi, :test_sets
+
+	def initialize
+		super
+		@magi = 'ParentCaretakerRelativeSpouse'
+		@test_sets = [
+			# {
+			# 	test_name: "FILL_IN_WITH_TEST_NAME",
+			# 	inputs: {
+			# 		"Applicant Relationships" => "",
+			# 		"Physical Household" => ?,
+			# 		"Applicant Parent Caretaker Category Indicator" => ?
+			# 	},
+			# 	configs: {
+			# 		"Option Caretaker Relative Relationship" => "00"
+			# 	},
+			# 	expected_outputs: {
+			# 		"Applicant Parent Caretaker Category Indicator" => ?,
+			# 		"Parent Caretaker Category Determination Date" => ?,
+			# 		"Parent Caretaker Category Ineligibility Reason" => ?
+			# 	}
+			# }
+		]
+	end
+end
+
+
+
+# def initialize(person, relationship_type, relationship_attributes) relationships
+
+# def initialize(household_id, people) household
