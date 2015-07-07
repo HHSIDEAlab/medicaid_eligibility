69928bc18239055fcd5804c0026e36c3070c82a2
colinxfleming <c3flemin@gmail.com>
2015-07-07 12:15:40 -0400

    stub qualified child; add referral type fixture

---
+++ test/fixtures/rule_fixtures/qualified_child.rb
@@ -0,0 +1,44 @@
+##### GENERATED AT 2015-07-07 12:06:44 -0400 ######
+class QualifiedChildFixture < MagiFixture
+	attr_accessor :magi, :test_sets
+
+	def initialize
+		super
+		@magi = 'QualifiedChild'
+		@test_sets = [
+		# 	{
+		# 		test_name: "FILL_IN_WITH_TEST_NAME",
+		# 		inputs: {
+		# 			"Caretaker Age" => ?,
+		# 			"Child Age" => ?,
+		# 			"Child Parents" => ?,
+		# 			"Physical Household" => ?,
+		# 			"Relationship Type" => ?,
+		# 			"Student Indicator" => ?
+		# 		},
+		# 		configs: {
+		# 			"Child Age Threshold" => ?,
+		# 			"Dependent Age Threshold" => ?,
+		# 			"Option Dependent Student" => ?,
+		# 			"Deprivation Requirement Retained" => ?,
+		# 			"Option Caretaker Relative Relationship" => ?,
+		# 			"State Unemployed Standard" => ?
+		# 		},
+		# 		expected_outputs: {
+		# 			"Child of Caretaker Dependent Age Indicator" => ?,
+		# 			"Child of Caretaker Dependent Age Determination Date" => ?,
+		# 			"Child of Caretaker Dependent Age Ineligibility Reason" => ?,
+		# 			"Child of Caretaker Deprived Child Indicator" => ?,
+		# 			"Child of Caretaker Deprived Child Determination Date" => ?,
+		# 			"Child of Caretaker Deprived Child Ineligibility Reason" => ?,
+		# 			"Child of Caretaker Relationship Indicator" => ?,
+		# 			"Child of Caretaker Relationship Determination Date" => ?,
+		# 			"Child of Caretaker Relationship Ineligibility Reason" => ?
+		# 		}
+		# 	}
+		]
+	end
+end
+
+# NOTES
+# no. -CF 7/7/2015
---
+++ test/fixtures/rule_fixtures/referral_type.rb
@@ -0,0 +1,113 @@
+##### GENERATED AT 2015-07-07 12:07:45 -0400 ######
+class ReferralTypeFixture < MagiFixture
+	attr_accessor :magi, :test_sets
+
+	def initialize
+		super
+		@magi = 'ReferralType'
+		@test_sets = [
+			{
+				test_name: "Referral Type - Age Over 65",
+				inputs: {
+					"Applicant Age" => 70,
+					"Applicant Attest Blind or Disabled" => "N",
+					"Applicant Attest Long Term Care" => "N",
+					"Medicare Entitlement Indicator" => "N",
+					"Receives SSI" => "N"
+				},
+				configs: {
+					# none
+				},
+				expected_outputs: {
+					"Applicant Medicaid Non-MAGI Referral Indicator" => "Y",
+					"Medicaid Non-MAGI Referral Ineligibility Reason" => 999
+				}
+			},
+			{
+				test_name: "Referral Type - Blind",
+				inputs: {
+					"Applicant Age" => 40,
+					"Applicant Attest Blind or Disabled" => "Y",
+					"Applicant Attest Long Term Care" => "N",
+					"Medicare Entitlement Indicator" => "N",
+					"Receives SSI" => "N"
+				},
+				configs: {
+					# none
+				},
+				expected_outputs: {
+					"Applicant Medicaid Non-MAGI Referral Indicator" => "Y",
+					"Medicaid Non-MAGI Referral Ineligibility Reason" => 999
+				}
+			},
+			{
+				test_name: "Referral Type - Long Term Care",
+				inputs: {
+					"Applicant Age" => 40,
+					"Applicant Attest Blind or Disabled" => "N",
+					"Applicant Attest Long Term Care" => "Y",
+					"Medicare Entitlement Indicator" => "N",
+					"Receives SSI" => "N"
+				},
+				configs: {
+					# none
+				},
+				expected_outputs: {
+					"Applicant Medicaid Non-MAGI Referral Indicator" => "Y",
+					"Medicaid Non-MAGI Referral Ineligibility Reason" => 999
+				}
+			},
+			{
+				test_name: "Referral Type - Medicare Entitlement",
+				inputs: {
+					"Applicant Age" => 40,
+					"Applicant Attest Blind or Disabled" => "N",
+					"Applicant Attest Long Term Care" => "N",
+					"Medicare Entitlement Indicator" => "Y",
+					"Receives SSI" => "N"
+				},
+				configs: {
+					# none
+				},
+				expected_outputs: {
+					"Applicant Medicaid Non-MAGI Referral Indicator" => "Y",
+					"Medicaid Non-MAGI Referral Ineligibility Reason" => 999
+				}
+			},
+			{
+				test_name: "Referral Type - SSI",
+				inputs: {
+					"Applicant Age" => 40,
+					"Applicant Attest Blind or Disabled" => "N",
+					"Applicant Attest Long Term Care" => "N",
+					"Medicare Entitlement Indicator" => "N",
+					"Receives SSI" => "Y"
+				},
+				configs: {
+					# none
+				},
+				expected_outputs: {
+					"Applicant Medicaid Non-MAGI Referral Indicator" => "Y",
+					"Medicaid Non-MAGI Referral Ineligibility Reason" => 999
+				}
+			},
+			{
+				test_name: "Referral Type - Fallback",
+				inputs: {
+					"Applicant Age" => 40,
+					"Applicant Attest Blind or Disabled" => "N",
+					"Applicant Attest Long Term Care" => "N",
+					"Medicare Entitlement Indicator" => "N",
+					"Receives SSI" => "N"
+				},
+				configs: {
+					# none
+				},
+				expected_outputs: {
+					"Applicant Medicaid Non-MAGI Referral Indicator" => "N",
+					"Medicaid Non-MAGI Referral Ineligibility Reason" => 108
+				}
+			}
+		]
+	end
+end
