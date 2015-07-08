4407756204965752331b954bb5c321d96abfa85c
colinxfleming <c3flemin@gmail.com>
2015-07-08 10:45:53 -0400

    commit a stub to switch to a different branch

--- test/fixtures/rule_fixtures/parent_caretaker_relative_spouse.rb
+++ test/fixtures/rule_fixtures/parent_caretaker_relative_spouse.rb
@@ -4,21 +4,25 @@ class ParentCaretakerRelativeSpouseFixture < MagiFixture
 
 	def initialize
 		super
+
+		person = Applicant.new('Person A','','','','')
+
 		@magi = 'ParentCaretakerRelativeSpouse'
 		@test_sets = [
 
 			{
 				test_name: "ParentCaretakerRelativeSpouse - ",
 				inputs: {
-					"Applicant Relationships" => [],
-					"Physical Household" => [],
+					"Applicant Relationships" => [Relationship.new("Person A", :spouse, ''), Relationship.new("Person B", :child, '')],
+					"Physical Household" => Household.new('Household A', [Applicant.new('Self','','','',''), Applicant.new('Person A','','','',''), Applicant.new('Person B', '','','','')]),
 					"Applicant Parent Caretaker Category Indicator" => "N"
 				},
 				configs: {
-					"Option Caretaker Relative Relationship" => "00"
+					"Option Caretaker Relative Relationship" => "02"
 				},
 				expected_outputs: {
-					# should be none for this 
+					"Applicant Parent Caretaker Category Indicator" => "Y",
+					"Parent Caretaker Category Ineligibility Reason" => 999
 				}
 			},
 
@@ -43,7 +47,7 @@ end
 
 
 
-# expected_outputs					
+# # expected_outputs					
 # "Applicant Parent Caretaker Category Indicator" => ?,
 # "Parent Caretaker Category Determination Date" => ?,
 # "Parent Caretaker Category Ineligibility Reason" => ?
