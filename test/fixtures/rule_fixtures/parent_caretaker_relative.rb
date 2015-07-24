1c3ddd483457cc1ee8033ad13e937db661dcee2a
colinxfleming <c3flemin@gmail.com>
2015-07-24 18:30:34 -0400

    initial beating parentcaretakerrelative fixture into partial shape

--- test/fixtures/rule_fixtures/parent_caretaker_relative.rb
+++ test/fixtures/rule_fixtures/parent_caretaker_relative.rb
@@ -5,55 +5,61 @@ class ParentCaretakerRelativeFixture < MagiFixture
   def initialize
     super
     @magi = 'ParentCaretakerRelative'
+
+    @parent = Applicant.new("Parent", '','','','')
+    @child = Applicant.new("Child", '','','','')
+    @household = Household.new('Household', [ @parent, @child ] )
+    @tax_return = TaxReturn.new(@parent, @child, '')
+
     @test_sets = [
-      # {
-      #   test_name: "Parent Caretaker - Applicant Child List is Empty",
-      #   inputs: {
-      #     "Person ID" => 1,
-      #     "Person List" => [],
-      #     "Physical Household" => [],
-      #     "Tax Returns" => [],
-      #     "Applicant Age" => 25,
-      #     "Applicant Relationships" => []
-      #   },
-      #   configs: {
-      #     "Child Age Threshold" => 19,
-      #     "Dependent Age Threshold" => 19,
-      #     "Option Dependent Student" => "N",
-      #     "Deprivation Requirement Retained" => "N",
-      #     "Option Caretaker Relative Relationship" => "00",
-      #     "State Unemployed Standard" => 100
-      #   },
-      #   expected_outputs: {
-      #     "Applicant Parent Caretaker Category Indicator" => "N",
-      #     "Parent Caretaker Category Ineligibility Reason" => 146,
-      #     "Qualified Children List" => []
-      #   }
-      # },
-      # {
-      #   test_name: "Parent Caretaker - Some Children Qualify",
-      #   inputs: {
-      #     "Person ID" => 1,
-      #     "Person List" => [],
-      #     "Physical Household" => [],
-      #     "Tax Returns" => [],
-      #     "Applicant Age" => 25,
-      #     "Applicant Relationships" => []
-      #   },
-      #   configs: {
-      #     "Child Age Threshold" => 19,
-      #     "Dependent Age Threshold" => 19,
-      #     "Option Dependent Student" => "N",
-      #     "Deprivation Requirement Retained" => "N",
-      #     "Option Caretaker Relative Relationship" => "00",
-      #     "State Unemployed Standard" => 100
-      #   },
-      #   expected_outputs: {
-      #     "Applicant Parent Caretaker Category Indicator" => "Y",
-      #     "Parent Caretaker Category Ineligibility Reason" => 999,
-      #     "Qualified Children List" => []
-      #   }
-      # }
+      {
+        test_name: "Parent Caretaker - Applicant Child List is Empty", #something in this is fucked up 
+        inputs: {
+          "Person ID" => "Parent",
+          "Person List" => @parent,
+          "Physical Household" => Household.new('Solo Household', [@parent]),
+          "Tax Returns" => [TaxReturn.new([@parent], nil,nil) ],
+          "Applicant Age" => 25,
+          "Applicant Relationships" => [Relationship.new(@parent, :self, '')]
+        },
+        configs: {
+          "Child Age Threshold" => 19,
+          "Dependent Age Threshold" => 19,
+          "Option Dependent Student" => "N",
+          "Deprivation Requirement Retained" => "N",
+          "Option Caretaker Relative Relationship" => "00",
+          "State Unemployed Standard" => 100
+        },
+        expected_outputs: {
+          "Applicant Parent Caretaker Category Indicator" => "N",
+          "Parent Caretaker Category Ineligibility Reason" => 146,
+          "Qualified Children List" => []
+        }
+      },
+      {
+        test_name: "Parent Caretaker - Some Children Qualify",
+        inputs: {
+          "Person ID" => "Person A",
+          "Person List" => [@parent, @child],
+          "Physical Household" => @household,
+          "Tax Returns" => [],
+          "Applicant Age" => 25,
+          "Applicant Relationships" => []
+        },
+        configs: {
+          "Child Age Threshold" => 19,
+          "Dependent Age Threshold" => 19,
+          "Option Dependent Student" => "N",
+          "Deprivation Requirement Retained" => "N",
+          "Option Caretaker Relative Relationship" => "00",
+          "State Unemployed Standard" => 100
+        },
+        expected_outputs: {
+          "Applicant Parent Caretaker Category Indicator" => "Y",
+          "Parent Caretaker Category Ineligibility Reason" => 999,
+          "Qualified Children List" => []
+        }
+      },
 
       {
         test_name: "Bad Info - Inputs",
