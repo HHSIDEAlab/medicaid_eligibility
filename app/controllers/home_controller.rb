89e481a799f2e4f868042f2fc70979ea4d8cc68f
colinxfleming <c3flemin@gmail.com>
2015-08-07 16:57:13 -0400

    httparty active states from the configs api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,16 @@
 class HomeController < ActionController::Base
   def index
     gon.restrictStates = ENV["RESTRICT_STATES"] == "true"
-    gon.enabledStates = ENV["ENABLED_STATES"] ? ENV["ENABLED_STATES"].split(';') : []
     gon.accessToken = ENV["ACCESS_TOKENS"] ? ENV["ACCESS_TOKENS"].split(';').first : "none"
+
+    states_from_api = HTTParty.get('http://mitc-configs.herokuapp.com/states/active').parsed_response.map {|x| x['state_cd'] } if Rails.env.production? 
+    gon.enabledStates = if states_from_api
+    	states_from_api
+    elsif ENV['ENABLED_STATES']
+    	ENV['ENABLED_STATES']
+    else
+    	[]
+    end
 
     respond_to do |format|
       format.html
