420d35b88380b141ed888e153e5a8bc4b9acc44a
Matt Williams <m.a.dubya@gmail.com>
2021-06-27 19:20:54 -0400

    serve precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ MedicaidEligibilityApi::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true #false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
